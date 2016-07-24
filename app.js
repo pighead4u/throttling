788468ffb9b8bbcb9c3cfa5511b571d4ff155eb8
pighead4u <zhuhuanhuan0422@gmail.com>
2016-07-24 22:14:22 +0800

    add:json web token dependency

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 var restify = require('restify');
 var bunyan = require('bunyan');
 var log = bunyan.createLogger({
-  name: "throttle",
-  src: true
+    name: "throttle",
+    src: true,
+    stream: process.stdout
 });
-var count = 0;
+
 function respond(req, res, next) {
-  req.log.info("test");
-  res.send('hello ' + req.params.name + count);
-  res.log.info("response");
-  next();
+    res.send('hello ' + req.params.name);
+    next();
 }
 
 var server = restify.createServer({
-  log: log
+    log: log,
+    body: true
 });
 
 server.use(restify.throttle({
-  burst: 2,
-  rate: 1,
-  ip: true
+    burst: 2,
+    rate: 1,
+    ip: true
 }));
 
-server.use(restify.requestLogger())
+server.use(restify.conditionalRequest());
+
+server.use(restify.requestLogger());
+
+server.on('after', restify.auditLogger({
+    log: log
+}));
 
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
+server.listen(8080, function () {
+    console.log('%s listening at %s', server.name, server.url);
 });
